3867d8b4ec8ccd074c082635a49cb6b2f9bd5b2e
Lmedeirossz <larissalimamedeirossz@gmail.com>
2025-06-07 10:46:22 -0300

    leitura

---
+++ leitura.py
@@ -0,0 +1,11 @@
+def main():
+  nome = input("Digite seu nome: ")
+  idade = int(input("Digite sua idade: "))
+  altura = float(input("Digite sua altura"))
+  isMasculino = input("Voce e do sexo masculino? (True/False")
+  print("ola", nome)
+  print("idade", idade)
+  print("altura", altura)
+  print ("E Masculino", isMasculino)  
+  return 0
+main()
